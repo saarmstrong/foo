adccbfa63160e077c37a57dd8595229c95266af6
Andy Armstrong <andyarmstro@gmail.com>
2016-05-04 13:18:09 -0700

    updated component html ; added nested component

--- src/app/foo.component.ts
+++ src/app/foo.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 
+import { BarComponent } from './bar';
+
 @Component({
   moduleId: module.id,
   selector: 'foo-app',
   templateUrl: 'foo.component.html',
-  styleUrls: ['foo.component.css']
+  styleUrls: ['foo.component.css'],
+  directives: [BarComponent]
 })
 export class FooAppComponent {
-  title = 'foo works!';
+    title = 'foo works!';
 }
